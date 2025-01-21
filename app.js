17fa8f31082be0da216af883655ea275b36e883c
Magdiel Rocha <33636082+magdielrocha@users.noreply.github.com>
2025-01-21 16:26:17 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ mensagensIniciais();
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1});
 }
 
 function mensagensIniciais() {
